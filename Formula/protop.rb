98b2425b1a7f04863e01d7dde1e84db9aa145136
Jeffery Shivers <jefferyshivers@gmail.com>
2020-03-14 17:35:23 -0400

    bump to version 0.4.5

--- Formula/protop.rb
+++ Formula/protop.rb
@@ -1,9 +1,9 @@
 class Protop < Formula
   desc "A command line tool for distributing protobufs."
   homepage "https://github.com/protop-io/protop"
-  url "https://github.com/protop-io/protop/releases/download/0.4.4/protop-0.4.4.tgz"
-  sha256 "b79c396942bd335710278be49d8b31677d59884f47d1e33f8ebb3373f4f89f1f"
-  version "0.4.3"
+  url "https://github.com/protop-io/protop/releases/download/0.4.5/protop-0.4.5.tgz"
+  sha256 "a37414f49a105765f4b10490975365f1716d76e29efdc9b229bf800b35fb0d57"
+  version "0.4.5"
 
   bottle :unneeded
 
